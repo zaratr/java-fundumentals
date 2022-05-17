71928924384041ee1d2baa12b11a7243c5925dfe
raul <zarater.raul@gmail.com>
2022-05-16 20:11:55 -0700

    added some init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public void main(String [] args)
+    {
+        new Main.run()
+        return;
+    }
+    public void run(){
+        System.out.println("hello");
+    }
+}
